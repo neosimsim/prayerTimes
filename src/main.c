cde1cf90f3f0eb6fe8a8c3ba9467e274b7dbb090
Alexander Ben Nasrallah <neosimsim@posteo.de>
2016-03-15 23:43:25 +0100

    Basic structure for astronomic calculations
    Future features will be implemented as .la library so everything can be
    linked and tested with unit tests. There libtools was added to autotools
    setup.

--- src/main.c
+++ src/main.c
@@ -15,18 +15,28 @@
  * along with prayerTimes. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "astro.h"
 #include "args.h"
 
 int
 main (int argc, char **argv)
 {
-  // Adding default location for Kaaba in Mecca.
+  // Default location: Kaaba in Mecca.
   arg_list.latitude = 21.422510f;
   arg_list.longitude = 39.826295f;
   parse_args(argc, argv);
   printf ("Bismillah al rahman al rahim.\n");
   printf ("latitude %f, longitude %f.\n", arg_list.latitude, arg_list.longitude);
+
+  // Test astro to know it linking etc. work. TODO delete later
+  time_t now = time(NULL);
+  struct tm *tm_now = localtime(&now);
+  const int length = 2 + 1 + 2 + 1 + 4 + 1 + 2 + 1 + 2 + 1 + 3;
+  char buffer[length];
+  strftime(buffer, length, "%d.%m.%Y %H:%M:%S", tm_now);
+  printf ("Julan date for %s is %f\n", buffer, julian_date(tm_now));
   return EXIT_SUCCESS;
 }
