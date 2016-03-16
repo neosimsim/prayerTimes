f417c1318666c1385bc40cf238aa1e056dc8c542
Alexander Ben Nasrallah <neosimsim@posteo.de>
2016-03-16 23:47:00 +0100

    Implemented running test runner

--- tests/check_astro.c
+++ tests/check_astro.c
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2016  Ben Nasrallah, Alexander <alexander.nasrallah@posteo.de>
+ *
  * This file is part of prayerTimes.
  *
  * prayerTimes is free software: you can redistribute it and/or modify
@@ -15,8 +17,43 @@
  * along with prayerTimes. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <check.h>
+#include <stdlib.h>
+#include "../src/astro.c"
+
+START_TEST(test_astro_julian_date)
+{
+}
+END_TEST
+
+Suite *
+astro_suite(void)
+{
+  Suite *s;
+  TCase *tc_core;
+
+  s = suite_create("Astro");
+
+  tc_core = tcase_create("Core");
+
+  tcase_add_test(tc_core, test_astro_julian_date);
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
+
 int
 main(void)
 {
-  return 0;
+  int number_failed;
+  Suite *s;
+  SRunner *sr;
+
+  s = astro_suite();
+  sr = srunner_create(s);
+
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
